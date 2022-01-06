aac0fd1896044ceb671a6c61911e346b8f415a86
samlev26 <71386538+samlev26@users.noreply.github.com>
2022-01-06 11:50:26 +0100

    Add files via upload

---
+++ projet.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html>
+   <head>
+      <title>Projet HTML</title>
+      <meta http-equiv = "refresh" content = "1; url = https://www.apple.com/app-store/" />
+   </head>
+   <body>
+      <p>Wait the redirection ...</p>
+   </body>
+</html>
